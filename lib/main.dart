952fd9a27d72ff540eac112dea5a3c79f477edb5
ay2022 <32273@163.com>
2019-05-31 16:31:51 +0800

    测试提交

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_loan_demo/ui/pages/login_page.dart';
+//import 'package:flutter_loan_demo/ui/pages/login_page.dart';
 import 'package:flutter_loan_demo/ui/pages/text_page.dart';
 
 void main() => runApp(MyApp());
 
+
+/*
+测试提交
+
+
+ */
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
