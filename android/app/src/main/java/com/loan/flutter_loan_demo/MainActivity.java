9fde995e819f057d78cf34e514af2f46cd046ca2
ay2022 <32273@163.com>
2019-05-31 16:28:45 +0800

    Initial commit

---
+++ android/app/src/main/java/com/loan/flutter_loan_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.loan.flutter_loan_demo;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
